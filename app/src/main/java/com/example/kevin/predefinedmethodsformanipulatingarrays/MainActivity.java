665dad271066898e9da2c70e04e2816d8aa5ee6d
kevin <kevinjobrien420@gmail.com>
2018-06-20 03:27:11 -0500

    217 App Number 25 - Part1 - PredefinedMethodsForManipulatingArrays //Coding

--- app/src/main/java/com/example/kevin/predefinedmethodsformanipulatingarrays/MainActivity.java
+++ app/src/main/java/com/example/kevin/predefinedmethodsformanipulatingarrays/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.kevin.predefinedmethodsformanipulatingarrays;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView txtArrayDoubleValues = (TextView)findViewById(R.id.txtArrayDoubleValues);
+        TextView txtIntNumbersArray = (TextView)findViewById(R.id.txtArrayIntegerNumbers);
+        TextView txtIntArary1 = (TextView)findViewById(R.id.txtIntArray1);
+        TextView txtIntArray1Copy = (TextView)findViewById(R.id.txtIntArray1Copy);
+        TextView txtLocationOfArrayElement = (TextView)findViewById(R.id.txtLocationArrayElement);
+        Switch swtEqual = (Switch) findViewById(R.id.swtEqual);
+
+        double[] doubleValuesArray = { 6.3, 1.2, 3.8, 2.7, 9.1, 4.3, 56.4, 34.2, 90.1, 12.4, 54.2, 79.1, 10.2 };
+
+
+
+
     }
 }
