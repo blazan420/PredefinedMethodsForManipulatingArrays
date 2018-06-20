ae869b3ecd8909349303192afb3a6e57f61e7c23
kevin <kevinjobrien420@gmail.com>
2018-06-20 04:07:35 -0500

    218 App Number 25 - Part2 - Predefined Methods for Manipulating Arrays //Coding

--- app/src/main/java/com/example/kevin/predefinedmethodsformanipulatingarrays/MainActivity.java
+++ app/src/main/java/com/example/kevin/predefinedmethodsformanipulatingarrays/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -22,8 +25,33 @@ public class MainActivity extends AppCompatActivity {
 
         double[] doubleValuesArray = { 6.3, 1.2, 3.8, 2.7, 9.1, 4.3, 56.4, 34.2, 90.1, 12.4, 54.2, 79.1, 10.2 };
 
+        Arrays.sort(doubleValuesArray);
+        String oldTxtArrayDoubleValues;
+        for (int index = 0; index < doubleValuesArray.length; index++) {
+            oldTxtArrayDoubleValues = txtArrayDoubleValues.getText().toString();
+            txtArrayDoubleValues.setText(oldTxtArrayDoubleValues + doubleValuesArray[index] + "      ");
+        }
 
 
+        int[] intNumbersArray = new int[20];
+        Arrays.fill(intNumbersArray, 1);
+        outputTheArrayToTheScreen(intNumbersArray, txtIntNumbersArray);
 
+        int[] integerArray1 = { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
+        int[] integerArray1Copy = new int[integerArray1.length];
+
+        System.arraycopy(integerArray1, 0, integerArray1Copy, 0, integerArray1.length);
+        outputTheArrayToTheScreen(integerArray1, txtIntArary1);
+        outputTheArrayToTheScreen(integerArray1Copy, txtIntArray1Copy);
+
+
+    }
+
+    public void outputTheArrayToTheScreen(int[] array, TextView textView) {
+        String oldTxtViewValue;
+        for (int number : array) {
+            oldTxtViewValue = textView.getText().toString();
+            textView.setText(oldTxtViewValue + number + "       ");
+        }
     }
 }
